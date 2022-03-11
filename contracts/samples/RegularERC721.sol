4ed618ac2364153b9aafb0bc5d304074cf69367b
Sangmoon Oh <halfface@chollian.net>
2022-03-12 04:11:42 +0900

    ...

--- contracts/samples/RegularERC721.sol
+++ contracts/samples/RegularERC721.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
-import "../../@openzeppelin/contracts-3/presets/ERC721PresetMinterPauserAutoId.sol";
+import "../../node_modules/@openzeppelin/contracts-3/presets/ERC721PresetMinterPauserAutoId.sol";
 
 /**
  * @author Sangmoon Oh
  */
-contract RegularERC721 is ERC721PresetMinterPauserAutoId{ 
+contract RegularERC721 is ERC721PresetMinterPauserAutoId{
 
     /**
      * @dev URI for contract-level (a.k.a. storefront-level) metadata
@@ -15,22 +15,22 @@ contract RegularERC721 is ERC721PresetMinterPauserAutoId{
 
     constructor(string memory name, string memory symbol) ERC721PresetMinterPauserAutoId(name, symbol, ""){
     }
-    
+
     function setTokenURI(uint256 tokenId, string memory uri) public {
         _setTokenURI(tokenId, uri);
     }
-    
+
     /**
      * Query URI for contract-level (a.k.a. storefront-level) metadata.
      * This function is defined ans consumed by OpenSea.
      * For more details, refer https://docs.opensea.io/docs/contract-level-metadata
-     * 
+     *
      * @return URI for contract-level metadata
      */
     function contractURI() public view returns (string memory) {
         return _contractURI;
     }
-    
+
     /**
      * Set URI for contract-level metadata
      */
