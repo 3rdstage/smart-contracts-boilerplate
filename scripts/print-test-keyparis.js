5d9688e40e568b01c2d12e2380eda8128922b98e
Sangmoon Oh <halfface@chollian.net>
2022-03-12 01:52:45 +0900

    ...

--- scripts/print-test-keyparis.js
+++ scripts/print-test-keyparis.js
@@ -1,21 +1,26 @@
 const { hdkey } = require('ethereumjs-wallet'); // https://www.npmjs.com/package/ethereumjs-wallet
+const { BN } = require('ethereumjs-util'); // https://www.npmjs.com/package/ethereumjs-util
 const bip39 = require("bip39"); // https://www.npmjs.com/package/bip39
 
+// https://github.com/ethereumjs/ethereumjs-wallet/tree/master/docs
+// https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/util/docs
 // https://dev.to/dongri/ethereum-wallet-sample-code-10o2
 const mnemonic = process.env.BIP39_MNEMONIC;
 const rootKey = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic));
 const parentKey = rootKey.derivePath("m/44'/60'/0'/0");
 
-const ethKeyPairs = {};
-ethKeyPairs.menomic = mnemonic;
-ethKeyPairs.baseKey = parentKey.getWallet().getPrivateKeyString();
-ethKeyPairs.keyPairs = [];
+const output = {};
+output.menomic = mnemonic;
+output.baseKey = parentKey.getWallet().getPrivateKeyString();
+output.keyPairs = [];
 for(let i = 0; i < 10; i++){
   let wallet = parentKey.deriveChild(i).getWallet();
   let keyPair = {};
   keyPair.privateKey = wallet.getPrivateKeyString();
+  keyPair.privateKeyInt = new BN(wallet.getPrivateKey(), 10).toString(10);
   keyPair.publicKey = wallet.getPublicKeyString();
+  keyPair.publicKeyInt = new BN(wallet.getPublicKey(), 10).toString(10);
   keyPair.address = wallet.getChecksumAddressString();
-  ethKeyPairs.keyPairs.push(keyPair);
+  output.keyPairs.push(keyPair);
 }
-console.log(JSON.stringify(ethKeyPairs, null, 2));
+console.log(JSON.stringify(output, null, 2));
