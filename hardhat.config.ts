e30663e7449f865353b19e071f726ceb2d90fbf8
Sangmoon Oh <halfface@chollian.net>
2023-05-31 15:53:03 +0900

    Updated NPM configuration and Hardhat configuration.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,15 @@
+// cspell:ignore nomiclabs sepolia
+
+require("@nomiclabs/hardhat-ethers");
+
+const fs = require('fs');
+const ganache = {};
+ganache.config = fs.readFileSync('scripts/ganache.properties').toString();
+ganache.chain = ganache.config.match(/ethereum.chainId=[0-9]*/g)[0].substring(17);
+ganache.host = ganache.config.match(/ethereum.host=.*/g)[0].substring(14);
+ganache.port = ganache.config.match(/ethereum.port=[0-9]*/g)[0].substring(14);
+const mnemonic = process.env.BIP39_MNEMONIC;
+
 // https://hardhat.org/hardhat-runner/docs/config
 const config: HardhatUserConfig = {
 
@@ -7,11 +19,34 @@ const config: HardhatUserConfig = {
     cache: "./temp/hardhat/cache"
   },
 
+  // https://hardhat.org/hardhat-runner/docs/config#networks-configuration
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/",
+      accounts: []
+    },
+    ganache: {
+      url: `http://${ganache.host}:${ganache.port}`,
+      //chainId: 3131,  // optional
+      accounts: {
+        mnemonic: mnemonic,
+        count: 10,
+      }
+    }
+  },
+
   // https://www.npmjs.com/package/solc?activeTab=versions
   solidity: {
     compilers: [
       {
-        version: "0.8.19"
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       },
       {
         version: "0.7.6"
@@ -20,7 +55,12 @@ const config: HardhatUserConfig = {
         version: "0.6.12"
       }
     ],
+  },
+
+  mocha: {
+    timeout: 40000
   }
+
 
 };
 
